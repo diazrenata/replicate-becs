67c21d0cac8f87462cc4579e99e84036d8053e8f
Renata Diaz <diaz.renatam@gmail.com>
2019-05-20 13:08:28 -0400

    ks test STRAIGHT FROM ZAR

--- R/delta_corrected_ks.R
+++ R/delta_corrected_ks.R
@@ -4,19 +4,33 @@
 #'
 #' @param bsd distribution
 #' @param delta_correction T/F use delta correction for small sample sizes
+#' @param focal_column name of column for the distribution to be evaluated
+#' @param expected_range vector of expected min and max values for uniform; defaults to min and max of measurement column
 #'
 #' @return list of signif to p = 0.05, d value, dcrit
 #'
 #' @export
 
-zar_ks_test <- function(bsd, delta_correction = F)
+zar_ks_test <- function(distribution, delta_correction = F, focal_column = "species_mean_mass", 
+                        expected_range = NULL)
 {
-  x = bsd %>%
-    dplyr::select(species_mean_mass)
+  
+  x = distribution %>%
+    dplyr::rename(measurement = focal_column)
+  
+  nvals = nrow(x)
+  
+  if(is.null(expected_range)) {
+    expected_min = min(x$measurement)
+    expected_max = max(x$measurement)
+  } else {
+    expected_min = min(expected_range)
+    expected_max = max(expected_range)
+  }
   
   rel_f = x %>%
-    dplyr::arrange(species_mean_mass) %>%
-    dplyr::group_by(species_mean_mass) %>%
+    dplyr::arrange(measurement) %>%
+    dplyr::group_by(measurement) %>%
     dplyr::mutate(val_freq = dplyr::n()) %>%
     dplyr::ungroup() %>%
     dplyr::distinct() %>%
@@ -29,58 +43,114 @@ zar_ks_test <- function(bsd, delta_correction = F)
   }
   
   rel_f = rel_f %>%
-    dplyr::mutate(rel_freq = cum_freq / nrow(bsd),
-                  exp_cum_freq = (species_mean_mass - min(species_mean_mass)),
-                  exp_rel_freq = exp_cum_freq / (max(species_mean_mass) - min(species_mean_mass)))
+    dplyr::mutate(rel_freq = cum_freq / nvals,
+                  exp_cum_freq = (measurement - expected_min),
+                  exp_rel_freq = exp_cum_freq / (expected_max - expected_min))
   
   if(delta_correction) {
     
+    d_crits = read.csv(paste0(here::here(), '/data/delta_kstable.csv'), stringsAsFactors = F)
+    
     rel_f = rel_f %>%
-      dplyr::mutate(rel_delta_freq = cum_freq / (nrow(bsd) + 1),
-                    rel_delta_freq1 = (cum_freq - 1) / (nrow(bsd) - 1))
+      dplyr::mutate(rel_delta_freq = cum_freq / (nvals + 1),
+                    rel_delta_freq1 = (cum_freq - 1) / (nvals - 1),
+                    d_0 = abs(rel_delta_freq - exp_rel_freq),
+                    d_1 = abs(rel_delta_freq1 - exp_rel_freq)) %>%
+      dplyr::select(d_0, d_1, i) %>%
+      tidyr::gather(key = 'delta', value = 'd', -i) %>%
+      dplyr::mutate(delta = ifelse(delta == 'd_0', 0, 1))
     
-    d_0 = abs(rel_f$rel_delta_freq - rel_f$exp_rel_freq) %>%
-      max()
-    d_1 = abs(rel_f$rel_delta_freq1 - rel_f$exp_rel_freq) %>%
-      max()
+    p_vals_d0 = find_ps(rel_f = rel_f, d_crits = d_crits, delta = 0, nvals = nvals)
     
-    dcrits = read.csv(paste0(here::here(), '/data/delta_kstable.csv'), stringsAsFactors = F)
-  
-    d_comparison = data.frame(dsubs = c(d_0, d_1), n = nrow(bsd), delta = c(0, 1)) %>%
-      dplyr::left_join(dcrits, by = c('n', 'delta')) %>%
-      dplyr::filter(dcrit < dsubs) %>%
-      dplyr::filter(alpha == min(alpha))
+    p_vals_d1 = find_ps(rel_f = rel_f, d_crits = d_crits, delta = 1, nvals = nvals)
     
-    if(nrow(d_comparison) > 0) {
-      signif = T
-      d = d_comparison$dsubs
-      dcrit = d_comparison$dcrit
+    if (p_vals_d0$p_min < p_vals_d1$p_min) {
+      p_vals = p_vals_d0
     } else {
-      signif = F
-      d = NULL
-      dcrit = NULL
+      p_vals = p_vals_d1
     }
     
   } else {
-  d = abs(rel_f$rel_freq - rel_f$exp_rel_freq)
-  
-  d2 = abs(dplyr::lag(rel_f$rel_freq, n= 1, default = 0) - rel_f$exp_rel_freq)
-  
-  d = c(d,d2)
-  d = max(d)
-  
-  dcrits = read.csv(paste0(here::here(), '/data/kstable.csv'))
-  
-  dcrit = dcrits$dcrit_05[which(dcrits$n == nrow(bsd))]
-  signif = (d >= dcrit)
-  
+    
+    d_crits = read.csv(paste0(here::here(), '/data/kstable.csv'))
+    
+    rel_f = rel_f %>%
+      dplyr::mutate(d = abs(rel_freq - exp_rel_freq),
+                    d2 = abs(dplyr::lag(rel_freq, n= 1, default = 0) - exp_rel_freq))
+    
+    p_vals = find_ps(rel_f = rel_f, d_crits = d_crits, nvals = nvals)
+    
   }
+  p_min = p_vals$p_min
+  p_max = p_vals$p_max
+  d = p_vals$d
   
-  results = list(signif = signif, d = d, dcrit = dcrit)
+  signif = (p_max <= 0.05)
   
+  results = list(signif = signif, p_max = p_max, p_min = p_min, d = d)
   
   return(results)
+  
 }
 
+#' find ps
+#'
+#' @description helper for ks test
+#' @param rel_f rel_f table
+#' @param d_crits d_crit table 
+#' @param deltaval 0, 1, or null for non-delta-corrected
+#' @param nvals sample size
+#'
+#' @return list of pvals
 
+find_ps = function(rel_f, d_crits, deltaval = NULL, nvals) {
+  
+  if(is.null(deltaval)) {
+    d = max(c(rel_f$d, rel_f$d2))
+    
+    d_comparison = d_crits %>%
+      dplyr::filter(n == nvals)
+    
+  } else {
+    rel_f = rel_f %>%
+      dplyr::filter(delta == deltaval)
+    
+    this_rel = rel_f %>%
+      dplyr::filter(d == max(d))
+    
+    d = this_rel$d
+    i = this_rel$i
+    
+    
+    d_comparison = d_crits %>%
+      dplyr::filter(n == i, delta == deltaval)
+    
+  }
+  
+  p_min = d_comparison %>%
+    dplyr::filter(dcrit > d) 
+  
+  if(nrow(p_min) >= 1) {
+    p_min = p_min %>%
+      dplyr::filter(alpha == max(alpha)) %>%
+      dplyr::select(alpha) %>%
+      as.numeric()
+  } else {
+    p_min = 0
+  }
+  
+  p_max = d_comparison %>%
+    dplyr::filter(dcrit < d)
+  
+  if(nrow(p_max) >= 1) {
+    p_max = p_max %>%
+      dplyr::filter(alpha == min(alpha)) %>%
+      dplyr::select(alpha) %>%
+      as.numeric()
+  } else {
+    p_max = 1
+  }
+  
+  return(list(p_min = p_min, p_max = p_max, d = d))
+}
 
