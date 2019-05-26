d72b8fb2e1ce0935793eebc53f147b6d5734bacb
Renata Diaz <diaz.renatam@gmail.com>
2019-05-26 08:27:14 -0400

    add compare summary stats

--- tests/testthat/test02_paper_results.R
+++ tests/testthat/test02_paper_results.R
@@ -5,3 +5,14 @@ test_that("loading paper results works", {
   expect_true(ncol(appendix_b) == 4)
   expect_true(nrow(appendix_b) == 36)
 })
+
+test_that("summary stats comparison works", {
+  
+  summary_stats_comparison = compare_summary_stats()
+  
+  expect_false(anyNA(summary_stats_comparison))
+  expect_true(min(summary_stats_comparison$new_richness) == 9)
+  expect_true(min(summary_stats_comparison$new_min_mass) == 4)
+  expect_true(max(summary_stats_comparison$new_max_mass) == 194.5)
+  
+})
